3e26730fe3548259722c8efb9ccad797128e436f
Giordanni <giordanniformiga103@gmail.com>
2025-12-02 09:56:07 -0300

    feat: creating mapper books

---
+++ src/main/java/com/giordanni/libraryapi/controller/mappers/BookMapper.java
@@ -0,0 +1,19 @@
+package com.giordanni.libraryapi.controller.mappers;
+
+import com.giordanni.libraryapi.dtos.books.RegistrationBookDTO;
+import com.giordanni.libraryapi.model.Book;
+import com.giordanni.libraryapi.repository.IAuthorRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+
+public abstract class BookMapper {
+
+    @Autowired
+    IAuthorRepository authorRepository;
+
+    @Mapping(expression = "java( authorRepository.findById(dto.idAuthor()).orElse(null) )", target = "author")
+    public abstract Book toEntity(RegistrationBookDTO dto);
+}
