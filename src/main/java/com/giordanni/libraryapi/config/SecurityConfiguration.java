3c17ec643f928ceb97a1545b1b80d0621059ba64
Giordanni <giordanniformiga103@gmail.com>
2025-12-05 10:22:19 -0300

    feat: implementando segurança com o password encoder e criando usuários padroes para testar a autenticação

--- src/main/java/com/giordanni/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/com/giordanni/libraryapi/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package com.giordanni.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,8 +28,41 @@ public class SecurityConfiguration {
                 }) // forma básica de login com formulário padrão
                 .httpBasic(Customizer.withDefaults()) // autenticação HTTP básica
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll(); // permitir acesso à página de login para todos os usuários
+                    authorize.requestMatchers("/authors/**").hasAnyRole( "ADMIN"); // apenas usuários com papel ADMIN podem acessar endpoints de autores
+                    authorize.requestMatchers("/books/**").hasAnyRole("USER", "ADMIN"); // usuários com papel USER ou ADMIN podem acessar endpoints de livros
+
+//                    authorize.requestMatchers(HttpMethod.POST, "/authors/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.PUT, "/authors/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.DELETE, "/authors/**").hasRole("ADMIN");
+//
+//                    authorize.requestMatchers(HttpMethod.GET, "/authors/**").hasAnyRole("USER", "ADMIN");
+
                     authorize.anyRequest().authenticated(); // todas as requisições exigem autenticação
+                    // nenhuma regra de requisição pode ser feita depois da anyRequest()
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails user1 = User.builder()
+                .username("user")
+                .password(encoder.encode("senha123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("senha456"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
