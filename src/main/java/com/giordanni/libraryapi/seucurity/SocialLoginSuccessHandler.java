ac21a81f375c5e2acdb5115020edd25ab7f21a7a
Giordanni <giordanniformiga103@gmail.com>
2025-12-09 14:57:07 -0300

    implementando lógica de caso o usuário não exista, poder criar com dados padrão

--- src/main/java/com/giordanni/libraryapi/seucurity/SocialLoginSuccessHandler.java
+++ src/main/java/com/giordanni/libraryapi/seucurity/SocialLoginSuccessHandler.java
@@ -8,16 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class SocialLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String PASSWORD_PLACEHOLDER = "321";
 
     private final UserService userService;
     @Override
@@ -31,14 +35,36 @@ public class SocialLoginSuccessHandler extends SavedRequestAwareAuthenticationSu
         OAuth2User oAuth2User = oauthToken.getPrincipal();
 
         String email = oAuth2User.getAttribute("email");
-        System.out.println(email);
 
         User user = userService.getByEmail(email);
+
+        if(user == null){
+            user = createUser(email);
+        }
+
         authentication = new CustomAuthentication(user);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         super.onAuthenticationSuccess(request, response, authentication);
 
+    }
+
+    private User createUser(String email) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(PASSWORD_PLACEHOLDER);
+        user.setRoles(List.of("OPERATOR"));
+        user.setLogin(getLoginFromEmail(email));
+
+        userService.saveUser(user);
+        return user;
+    }
+
+    private String getLoginFromEmail(String email) {
+        if (email != null && email.contains("@")) {
+            return email.substring(0, email.indexOf("@"));
+        }
+        return email;
     }
 }
