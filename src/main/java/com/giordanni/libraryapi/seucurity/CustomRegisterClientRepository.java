59dae233cfc00ec490b5dc35e39dc713d6d06e3b
Giordanni <giordanniformiga103@gmail.com>
2025-12-20 20:19:05 -0300

    feat: configurando register client repository para poder verificar se o client está cadastrado e se passou os dados corretos

---
+++ src/main/java/com/giordanni/libraryapi/seucurity/CustomRegisterClientRepository.java
@@ -0,0 +1,42 @@
+package com.giordanni.libraryapi.seucurity;
+
+import com.giordanni.libraryapi.services.ClientServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisterClientRepository implements RegisteredClientRepository {
+
+    private final ClientServices clientServices;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        var client = clientServices.getByClientId(clientId);
+        if (client == null) return null;
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectUri())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                // .tokenSettings() // posso fazer configurações de tokens aqui
+                .build();
+    }
+}
