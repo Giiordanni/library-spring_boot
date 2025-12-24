d3a5c05bf509feea2950a264f6d0c337d1f4b2fd
Giordanni <giordanniformiga103@gmail.com>
2025-12-24 17:22:53 -0300

    feat: estruturando o swagger para poder passar o token JWT

--- src/main/java/com/giordanni/libraryapi/config/OpenApiConfiguration.java
+++ src/main/java/com/giordanni/libraryapi/config/OpenApiConfiguration.java
@@ -1,8 +1,12 @@
 package com.giordanni.libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,7 +20,17 @@ import org.springframework.context.annotation.Configuration;
                         email = "giordanniformiga103@gmail.com",
                         url = "https://github.com/Giiordanni"
                 )
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth") // Nome do esquema de segurança definido na configuração do OpenAPI
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
 }
